609287d7a54b8e35a9876a3313f43a4cbb3a61a6
kridel <adagascaou.christian@gmail.com>
2022-09-07 20:20:56 +0200

    read function implemented, albeit ugly

--- source/secwey.d
+++ source/secwey.d
@@ -8,8 +8,8 @@
  * >>> Contest rules:
  * > No usage of pre-made CSV-parsing code
  * 	 (whether it's from the language's stdlib or from a 3rd party source / lib)
- * > The software must be able to alter the file's cells, headers included
- * > The software must be able to delete individual rows, headers excluded
+ * > The software must be able to alter the file's cells, header cells included
+ * > The software must be able to delete individual rows, header row excluded
  *
  * >>> Contest bonuses:
  * > Column deletion
@@ -96,22 +96,23 @@ struct FileInfo {
     int cellCount       = 0;
     int rowCount        = 0;
     int charCount       = 0;
-    bool headersInFile  = false;
+    bool headerInFile   = false;
 }
 
 // Validates arguments provided on program execution
 void validateArgs(string[] args, ref FileInfo loadedFileInfo, ref string task) {
-    string[] possibleTasks = [
-        "free", "freemode", "f",
-        "create", "c",
-        "read", "r",
-        "update", "u",
-        "delete", "d"
+    auto possibleTasks = [
+        // Nope. Not yet. Maybe I'll do this later.
+        // "free":  ["free", "freemode", "f", "normal", "?"],
+        "create":   ["create", "c", "insert", "append", "yank"],
+        "read":     ["read", "r", "retrieve", "see", "value", "what", "whatis"],
+        "update":   ["update", "u", "replace", "change"],
+        "delete":   ["delete", "d", "suppress", "nuke", "prune"]
     ];
 
     auto headerArgValues = [
-        "yes": ["yes", "y", "true", "yeah", "aye", "oui"],
-        "no": ["no", "n", "false", "nope", "nah", "non"]
+        "yes":  ["yes", "y", "true", "yeah", "aye", "oui"],
+        "no":   ["no", "n", "false", "nope", "nah", "non"]
     ];
 
     auto csvFileRegex = (r"^.*\.(csv)");
@@ -139,7 +140,17 @@ void validateArgs(string[] args, ref FileInfo loadedFileInfo, ref string task) {
         "header", &headerArg
     );
     
-    if (!possibleTasks.canFind(task)) {
+    bool taskIsValid = false;
+    foreach (taskAliasCur, taskAliasGroup; possibleTasks) {
+        if (taskAliasGroup.canFind(task)) {
+            taskIsValid = true;
+            // Transform the alias into the proper internal task name
+            task = taskAliasCur;
+            break;
+        }
+    }
+
+    if (!taskIsValid) {
         writeln("[WARN] Provided task argument is "
             ~ (task == "" ? "empty" : "invalid") ~ "." 
             ~ "\n>>>>>> Reverting secway to free mode."
@@ -152,7 +163,7 @@ void validateArgs(string[] args, ref FileInfo loadedFileInfo, ref string task) {
             ~ "\n>>>>>> Secway will assume there's no header in the file."
         );
     } else if (headerArgValues["yes"].canFind(headerArg)) {
-        loadedFileInfo.headersInFile = true;
+        loadedFileInfo.headerInFile = true;
     } else {
         if (!headerArgValues["no"].canFind(headerArg)) {
             writeln("[WARN] The provided header argument is invalid."
@@ -163,18 +174,136 @@ void validateArgs(string[] args, ref FileInfo loadedFileInfo, ref string task) {
 }
 
 // Ensure that the file is a valid CSV file as per the IETF RFC 4180 standard
-void validateFile(ref FileInfo loadedFileInfo, FileHandler fileHandler) {
+void validateFile(ref FileInfo loadedFileInfo, ref FileHandler fileHandler) {
     // Deliberately, I won't pass a file pointer throught the whole of Secway.
     // Instead, I'm just going to open and close the CSV file only when
     //  operations ought to be performed. (e.g. in FileHandler.parseContents())
     fileHandler.parseContents(loadedFileInfo);
 
     writeln("\n  Info about " ~ loadedFileInfo.filename ~ ":\n"
-        ~ "\n  Character count (excluding separators and CL/RFs)\n> " ~ to!string(loadedFileInfo.charCount)
-        ~ "\n  Cell count\n> " ~ to!string(loadedFileInfo.cellCount)
-        ~ "\n  Row count\n> " ~ to!string(loadedFileInfo.rowCount)
-        ~ "\n  Headers? (user-specified)\n> " ~ (loadedFileInfo.headersInFile ? "yes" : "no")
+        ~ "\n* Character count (excluding separators and CL/RFs)\n  " ~ to!string(loadedFileInfo.charCount)
+        ~ "\n* Cell count\n  " ~ to!string(loadedFileInfo.cellCount)
+        ~ "\n* Row count\n  " ~ to!string(loadedFileInfo.rowCount)
+        ~ "\n* Header row? (user-specified)\n  " ~ (loadedFileInfo.headerInFile ? "yes" : "no")
     );
+}
+
+// Integer helper: When you need a helping hand!
+// Be careful to feed the input stripped of CR/LFs
+string isUserInputNumeric(char[] input) {
+    // Input has the expected value but isNumeric() always returns false? WTF?
+    if (std.string.isNumeric(to!string(input))) {
+        int numInput = to!int(input);
+        writeln(numInput);
+        if (numInput < 0) {
+            return "NEG";
+        } else if (numInput == 0) {
+            return "ZERO";
+        } else {
+            return "POS";
+        }
+    } else {
+        return "NONE";
+    }
+}
+
+// Does not exit until the user feeds a non-negative integer to stdin
+ulong forceUintInput(bool zeroAllowed = false) {
+    char[] buf;
+    bool firstTry = true;
+    string inputType;
+
+    // TODO: Hmmmmm yes, delicious redundency. Shorten that ASAP.
+    if (zeroAllowed) {
+        while (inputType != "POS" && inputType != "ZERO") {
+            if (!firstTry)
+                write("\nPlease input zero or a positive integer.\n> ");
+            readln(buf);
+            buf = std.string.chomp(buf); // pruning CR/LFs from the user input
+            inputType = isUserInputNumeric(buf);
+            firstTry = false;
+        }
+    } else {
+        while (inputType != "POS") {
+            if (!firstTry)
+                write("\nPlease input a positive and non-zero integer.\n> ");
+            readln(buf);
+            buf = std.string.chomp(buf); // pruning CR/LFs from the user input
+            inputType = isUserInputNumeric(buf);
+            firstTry = false;
+        }
+    }
+
+    return to!ulong(buf);
+}
+
+void read(ref FileInfo loadedFileInfo, ref FileHandler fileHandler) {
+    // TODO: Allow the user to enter keywords instead of numbers, such as
+    //  "first", "last", etc
+    // I can sense the sweet, harrowing presence of feature creep getting closer
+    ulong rowId, cellId = 0;
+    string[] rowToRead = [];
+
+    write("Please enter the row number to look up.\n> ");
+    RowIdInput:
+    rowId = forceUintInput();
+
+    if (rowId > fileHandler.fileContents.length) {
+        writeln("[WARN] The row number you provided exceeds the actual number of rows in the file."
+            ~ "\n       Please input a valid row number.\n> "
+        );
+        goto RowIdInput;
+    } else {
+        rowToRead = fileHandler.fileContents[rowId - 1];
+    }
+
+    write("\nPlease enter the cell position to look up.\n"
+        ~ "Enter zero to display the whole row.\n> "
+    );
+    CellIdInput:
+    cellId = forceUintInput(true);
+
+    if (cellId > fileHandler.fileContents[rowId - 1].length) {
+        write("[WARN] The cell number you provided exceeds the actual number of cells in the row you selected."
+            ~ "\n       Please input a valid cell number.\n> "
+        );
+        goto CellIdInput;
+    }
+
+    
+    if (cellId == 0) {
+        ulong cellCount = 0;
+        foreach (cellContents; rowToRead) {
+            cellCount++;
+            write(std.string.strip(cellContents)
+                ~ (cellCount >= rowToRead.length ? "\n" : ",")
+            );
+        }
+    } else {
+        writeln(std.string.strip(rowToRead[cellId - 1]));
+    }
+}
+
+void performTask(string task, ref FileInfo loadedFileInfo, ref FileHandler fileHandler) {
+    // TODO: Allow user to follow up with another task after one is done
+    writeln("\nOperation selected: " ~ std.string.capitalize(task) ~ ".");
+
+    switch (task) {
+        // -=-=- For rows -=-=-
+        case "create":
+            goto default;
+        case "delete":
+            goto default;
+        // -=-=- For cells -=-=-
+        case "update":
+            goto default;
+        // -=-=- For both rows and cells -=-=-
+        case "read":
+            read(loadedFileInfo, fileHandler);
+            goto default;
+        default:
+            break;
+    }
 }
 
 void main(string[] args) {
@@ -184,4 +313,5 @@ void main(string[] args) {
 
 	validateArgs(args, *loadedFileInfo, task);
     validateFile(*loadedFileInfo, *fileHandler);
+    performTask(task, *loadedFileInfo, *fileHandler);
 }
